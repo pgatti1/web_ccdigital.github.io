895724a64373f71031297a7384e5c50ecd3bbcd4
pgatti1 <31253325+pgatti1@users.noreply.github.com>
2024-02-16 14:27:04 -0300

    Add files via upload

---
+++ planes.php
@@ -0,0 +1,10 @@
+<section id="encabezado" class="bck-gradient py-3 text-center text-white px-3">
+    <h2 class="pt-5 pb-1 display-5 fw-bold">Nuestros planes</h2>
+    <p class="pb-5">Seleccioná el plan que se ajuste mejor a tus necesidades.</p>
+</section>
+<section id="planes" class="py-5 px-3">
+    <div class="container">
+        <h2 class="display-6 fw-bold mb-3">Nuestra filosofía</h2>
+        <p>Los rostros detrás de nuestro trabajo.</p>
+    </div>
+</section>
